b02806cc51d3a7735ebe223cf28b147af6d0e1b0
Rishab Lal <lal.rishab@gmail.com>
2017-01-17 21:56:16 -0500

    1) Modularized implementation; looks a lot cleaner now.  TODO: create deployment file

--- DomainGetterUI.py
+++ DomainGetterUI.py
@@ -1,124 +1,143 @@
+import socket
+import errno
 import xlsxwriter
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 import os.path
 import whois
-
-root = Tk()
-root.wm_title("Domain Getter")
-root.grid()
-
-singleMultiVar = IntVar()
-
-def singleMultiToggle():
-	if (singleMultiVar == 1):
-		print("hello")
-
-#singleRadioBtn = Radiobutton(root, variable = singleMultiVar, value = 1, 
-#							 command = singleMultiToggle).grid(row = 0, column = 0)
-
-singleDomainLabel = Label(root, text="Single Domain Entry: ").grid(row = 0, column = 0)
-
-singleDomainEntryContent = StringVar()
-singleDomainEntry = Entry(root, textvariable = singleDomainEntryContent).grid(row = 0, column = 1)
-
-def getSingleDomainBtnClick():
-	domainInput = singleDomainEntryContent.get()
-	result = whois.whois(domainInput)
-	messagebox.showinfo("Results", result if result.domain_name is not None else "Not Owned \n\nNOTE: simpler domain queries such as \"bing.com\" yield more accurate results than \"http://www.bing.com\"")
-
-getSingleDomainBtn = Button(root, text="Get Info", command=getSingleDomainBtnClick).grid(row = 0, column = 2)
+import time
 
 
+class DomainGetter(Frame):
+	def __init__(self):
+		Frame.__init__(self)
+		self.master.title("Domain Getter")
+		self.master.iconbitmap('assets/windowIcon.ico')
+
+		# initialize grid
+		self.master.rowconfigure(2, weight = 1)
+		self.master.columnconfigure(4, weight = 1)
+		self.grid(sticky = W + E + N + S)
+
+		# initialize grid contents
+
+		# single domain query objects
+		self.singleDomainLabel = Label(self, text = "Single Domain Entry: ")
+		self.singleDomainLabel.grid(row = 0, column = 0)
+
+		self.singleDomainEntryContent = StringVar()
+		self.singleDomainEntry = Entry(self, textvariable = self.singleDomainEntryContent)
+		self.singleDomainEntry.grid(row = 0, column = 1)
+
+		self.getSingleDomainBtn = Button(self, text = "Get Info", command = self.getSingleDomainBtnClick)
+		self.getSingleDomainBtn.grid(row = 0, column = 2)
+
+		# multi domain query objects
+		self.multiDomainLabel = Label(self, text = "Multi Domain Entry: ")
+		self.multiDomainLabel.grid(row = 1, column = 0)
+
+		self.multiDomainEntryContent = StringVar()
+		self.multiDomainEntry = Entry(self, textvariable = self.multiDomainEntryContent)
+		self.multiDomainEntry.grid(row = 1, column = 1)
+
+		self.browseFileBtn = Button(self, text = "Browse", command = self.browseFile)
+		self.browseFileBtn.grid(row = 1, column = 2)
+
+		self.getMultiDomainBtn = Button(self, text = "Export to Excel", command = self.getMultiDomainBtnClick)
+		self.getMultiDomainBtn.grid(row = 1, column = 3)
 
 
-# multi entry through file
+	def getSingleDomainBtnClick(self):
+		domainInput = self.singleDomainEntryContent.get()
+		result = whois.whois(domainInput)
+		messagebox.showinfo("Results", result if result.domain_name is not None else "Not Owned \n\nNOTE: simpler domain queries such as \"bing.com\" yield more accurate results than \"http://www.bing.com\"")
 
 
+	def browseFile(self):
+		filePath = filedialog.askopenfilename(filetypes = (("Text files", "*.txt"),
+	                                   				       ("All files", "*.*") ))
+		if (filePath):
+			self.multiDomainEntryContent.set(filePath)
 
-#multiRadioBtn = Radiobutton(root, variable = singleMultiVar, value = 2,
-#							command = singleMultiToggle).grid(row = 1, column = 0)
 
-multiDomainLabel = Label(root, text = "Multi Domain Entry: ").grid(row = 1, column = 0)
+	def createOutputExcelFile(self, outputExcelFile, givenDomainNames, domainInfo):
+		wb = xlsxwriter.Workbook(outputExcelFile)
+		ws = wb.add_worksheet()
 
-multiDomainEntryContent = StringVar()
-multiDomainEntry = Entry(root, textvariable = multiDomainEntryContent).grid(row = 1, column = 1)
+		headerFormat = wb.add_format({'bold': True})
+		dateFormat = wb.add_format({'num_format': 'mmmm d yyyy'})
+		notOwnedFormat = wb.add_format()
+		notOwnedFormat.set_pattern(1)
+		notOwnedFormat.set_bg_color('red')
 
-def browseFile():
-	filePath = filedialog.askopenfilename(filetypes = (("Text files", "*.txt"),
-                                   				       ("All files", "*.*") ))
-	if (filePath):
-		multiDomainEntryContent.set(filePath)
+		ws.set_column(0, 0, 20)
+		ws.set_column(2, 4, 20)
+		ws.set_column(5, 6, 60)
+		ws.set_column(7, 9, 25)
 
-browseFileBtn = Button(root, text = "Browse", command = browseFile).grid(row = 1, column = 2)
+		headers = ["Input Domain Name", "Owned", "Domain Names", "Registrar", "Name", "Emails", "Address", "Updated Dates", "Creation Date", "Expiration Date"]
+		ws.write_row(0, 0, headers, headerFormat)
 
-def createOutputExcelFile(outputExcelFile, givenDomainNames, domainInfo):
-	wb = xlsxwriter.Workbook(outputExcelFile)
-	ws = wb.add_worksheet()
+		for idx, domain in enumerate(givenDomainNames):
+			dInfo = domainInfo[idx]
 
-	headerFormat = wb.add_format({'bold': True})
-	dateFormat = wb.add_format({'num_format': 'mmmm d yyyy'})
-	notOwnedFormat = wb.add_format()
-	notOwnedFormat.set_pattern(1)
-	notOwnedFormat.set_bg_color('red')
+			if (dInfo.domain_name is None):
+				ws.write(idx + 1, 0, domain, notOwnedFormat)
+				ws.write(idx + 1, 1, "No", notOwnedFormat)
+			else:
+				ws.write(idx + 1, 0, domain)
+				ws.write(idx + 1, 1, "Yes")
+				ws.write(idx + 1, 2, str(dInfo.domain_name))
+				ws.write(idx + 1, 3, str(dInfo.registrar))
+				ws.write(idx + 1, 4, str(dInfo.name))
+				ws.write(idx + 1, 5, str(dInfo.emails))
+				ws.write(idx + 1, 6, dInfo.address + ", " + dInfo.city + ", " + dInfo.state + " " + dInfo.zipcode if dInfo.address is not None else str(dInfo.address))
+				ws.write(idx + 1, 7, str(dInfo.updated_date) if dInfo.updated_date is None else "; ".join([d.strftime("%B %d, %Y") for d in dInfo.updated_date]) if type(dInfo.updated_date) is list else dInfo.updated_date.strftime("%B %d, %Y"))
+				ws.write(idx + 1, 8, str(dInfo.creation_date) if dInfo.creation_date is None else "; ".join([d.strftime("%B %d, %Y") for d in dInfo.creation_date]) if type(dInfo.creation_date) is list else dInfo.creation_date.strftime("%B %d, %Y"))
+				ws.write(idx + 1, 9, str(dInfo.expiration_date) if dInfo.expiration_date is None else "; ".join([d.strftime("%B %d, %Y") for d in dInfo.expiration_date]) if type(dInfo.expiration_date) is list else dInfo.expiration_date.strftime("%B %d, %Y"))
 
-	ws.set_column(0, 0, 20)
-	ws.set_column(2, 4, 20)
-	ws.set_column(5, 6, 60)
-	ws.set_column(7, 9, 25)
+		wb.close()
 
-	headers = ["Input Domain Name", "Owned", "Domain Names", "Registrar", "Name", "Emails", "Address", "Updated Dates", "Creation Date", "Expiration Date"]
-	ws.write_row(0, 0, headers, headerFormat)
 
-	for idx, domain in enumerate(givenDomainNames):
-		dInfo = domainInfo[idx]
+	def getMultiDomainBtnClick(self):
+		try:
+			self.getMultiDomainBtn.config(text = "Working...")
+			self.getMultiDomainBtn.update_idletasks()
 
-		if (dInfo.domain_name is None):
-			ws.write(idx + 1, 0, domain, notOwnedFormat)
-			ws.write(idx + 1, 1, "No", notOwnedFormat)
-		else:
-			ws.write(idx + 1, 0, domain)
-			ws.write(idx + 1, 1, "Yes")
-			ws.write(idx + 1, 2, str(dInfo.domain_name))
-			ws.write(idx + 1, 3, str(dInfo.registrar))
-			ws.write(idx + 1, 4, str(dInfo.name))
-			ws.write(idx + 1, 5, str(dInfo.emails))
-			ws.write(idx + 1, 6, dInfo.address + ", " + dInfo.city + ", " + dInfo.state + " " + dInfo.zipcode if dInfo.address is not None else str(dInfo.address))
-			ws.write(idx + 1, 7, str(dInfo.updated_date) if dInfo.updated_date is None else "; ".join([d.strftime("%B %d, %Y") for d in dInfo.updated_date]) if type(dInfo.updated_date) is list else dInfo.updated_date.strftime("%B %d, %Y"))
-			ws.write(idx + 1, 8, str(dInfo.creation_date) if dInfo.creation_date is None else "; ".join([d.strftime("%B %d, %Y") for d in dInfo.creation_date]) if type(dInfo.creation_date) is list else dInfo.creation_date.strftime("%B %d, %Y"))
-			ws.write(idx + 1, 9, str(dInfo.expiration_date) if dInfo.expiration_date is None else "; ".join([d.strftime("%B %d, %Y") for d in dInfo.expiration_date]) if type(dInfo.expiration_date) is list else dInfo.expiration_date.strftime("%B %d, %Y"))
+			filePath = self.multiDomainEntryContent.get()
+			if (os.path.isfile(filePath)):
+				with open(filePath) as f:
+					fileContent = [x.strip('\n').strip() for x in f.readlines()]
 
-	wb.close()
+				domainInfo = [whois.whois(x) for x in fileContent]
+				fileDirectory = os.path.dirname(filePath)
+				outputExcelFile = fileDirectory + "/DomainInfo"
 
-multiDomainBtnContent = StringVar()
+				# if the output file name is already taken
+				if (os.path.isfile(outputExcelFile + ".xlsx")):
+					fileNum = 1
+					while (os.path.isfile(outputExcelFile + str(fileNum) + ".xlsx")):
+						fileNum += 1
+					outputExcelFile = outputExcelFile + str(fileNum) + ".xlsx"
+				else:
+					outputExcelFile = outputExcelFile + ".xlsx"
 
-def getMultiDomainBtnClick():
-	multiDomainBtnContent.set("Working...")
-	filePath = multiDomainEntryContent.get()
-	if (os.path.isfile(filePath)):
-		with open(filePath) as f:
-			fileContent = [x.strip('\n').strip() for x in f.readlines()]
+				self.createOutputExcelFile(outputExcelFile, fileContent, domainInfo)
+			else:
+				messagebox.showinfo("WARNING: Invalid File Path", "The file path provided seems to be invalid, please review and try again.\nIf the file path is correct and you still receive this error, please contact the developer.")
 
-		domainInfo = [whois.whois(x) for x in fileContent]
-		fileDirectory = os.path.dirname(filePath)
-		outputExcelFile = fileDirectory + "/DomainInfo"
+		except socket.error as e:
+			if e.errno == errno.WSAECONNRESET:
+				reconnect()
+				retry_action()
+			else:
+				raise
+		except:
+			messagebox.showinfo("Error", "Something went wrong. Make sure the path you entered is correct and try again.\nIf the error continues, please contact the developer.")
 
-		if (os.path.isfile(outputExcelFile + ".xlsx")):
-			fileNum = 1
-			while (os.path.isfile(outputExcelFile + str(fileNum) + ".xlsx")):
-				fileNum += 1
+		self.getMultiDomainBtn.config(text = "Export to Excel")
+		messagebox.showinfo("Success", "Results can be found here:\n\n" + outputExcelFile)
 
-			outputExcelFile = outputExcelFile + str(fileNum) + ".xlsx"
-
-		createOutputExcelFile(outputExcelFile, fileContent, domainInfo)
-	else:
-		messagebox.showinfo("WARNING: Invalid File Path", "The file path provided seems to be invalid, please review and try again.\nIf the file path is correct and you still receive this error, please contact the developer.")
-
-	multiDomainBtnContent.set("Export to Excel")
-
-multiDomainBtnContent.set("Export to Excel")
-getMultiDomainBtn = Button(root, text = multiDomainBtnContent.get(),
-					       command = getMultiDomainBtnClick).grid(row = 1, column = 3)
-
-root.mainloop()
+if __name__ == "__main__":
+	DomainGetter().mainloop()
